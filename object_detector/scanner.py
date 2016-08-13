7504dc7e648c799bbd3c220a01a44d230834bfe1
jeongjoonsup <penny4860@gmail.com>
2016-08-13 20:24:24 +0900

    simplified ImageScanner class code

--- object_detector/scanner.py
+++ object_detector/scanner.py
@@ -4,7 +4,6 @@ import cv2
 
 class ImageScanner(object):
     def __init__(self, image):
-        self._image = image
         self._layer = image
     
     def get_next_patch(self, step_size=(10, 10), window_size=(30, 30)):
@@ -13,21 +12,19 @@ class ImageScanner(object):
                 yield (x, y, self._layer[y:y + window_size[1], x:x + window_size[0]])
     
     def get_next_layer(self, scale=0.7, min_size=(30, 30)):
-        layer = self._image
-        self._layer = layer
-        yield layer
+        yield self._layer
 
         while True:
-            h = int(layer.shape[0] * scale)
-            w = int(layer.shape[1] * scale)
-            layer = cv2.resize(layer, (w, h))
+            h = int(self._layer.shape[0] * scale)
+            w = int(self._layer.shape[1] * scale)
+            
+            self._layer = cv2.resize(self._layer, (w, h))
             
             min_h = min_size[0]
             min_w = min_size[1]
             if h < min_h or w < min_w:
                 break
-            self._layer = layer
-            yield layer
+            yield self._layer
     
     
 if __name__ == "__main__":
