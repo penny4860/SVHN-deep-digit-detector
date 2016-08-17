b6037495aaa2acfff28be0457038bbd402bdc492
jeongjoonsup <penny4860@gmail.com>
2016-08-17 20:48:43 +0900

    crop_random() added

--- object_detector/utils.py
+++ object_detector/utils.py
@@ -1,7 +1,8 @@
-
+#-*- coding: utf-8 -*-
 import cv2
- 
-def crop_bb(image, bb, padding=10, dstSize=(32, 32)):
+import sklearn.feature_extraction.image as skimg
+
+def crop_bb(image, bb, padding=10, dst_size=(32, 32)):
     
     h = image.shape[0]
     w = image.shape[0]
@@ -12,8 +13,34 @@ def crop_bb(image, bb, padding=10, dstSize=(32, 32)):
     (x2, y2) = (min(x2 + padding, w), min(y2 + padding, h))
     
     roi = image[y1:y2, x1:x2]
-    roi = cv2.resize(roi, dstSize, interpolation=cv2.INTER_AREA)
+    roi = cv2.resize(roi, dst_size, interpolation=cv2.INTER_AREA)
  
     return roi
 
+def crop_random(image, dst_size=(32, 32), max_patches=5):
+    
+    patches = skimg.extract_patches_2d(image, 
+                                       dst_size,
+                                       max_patches=max_patches)
+    return patches
+
+
+if __name__ == "__main__":
+
+    import numpy as np
+    one_image = np.arange(100).reshape((10, 10))
+    
+    print one_image
+    patches = crop_random(one_image, (4,4), 4)
+    
+    print patches[0]
+    print patches[1]
+    print patches[2]
+    print patches[3]
+    
+    
+
+
+
+    
 
