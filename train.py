b0c739335a4e54d000ba7583a0b5cd2c7c8c27ca
jeongjoonsup <penny4860@gmail.com>
2016-08-20 15:37:11 +0900

    training driver implemented

---
+++ train.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+
+import object_detector.file_io as file_io
+import object_detector.classifier as classifier
+
+CONFIGURATION_FILE = "conf/cars.json"
+
+if __name__ == "__main__":
+    
+    conf = file_io.FileJson().read(CONFIGURATION_FILE)
+    
+    #1. Load Features and Labels
+    data = file_io.FileHDF5().read(conf["features_path"], conf["db_name"])
+    y = data[:, 0]
+    X = data[:, 1:]
+    
+    #2. Train
+    cls = classifier.LinearSVM(C=conf["C"], random_state=111)
+    cls.train(X, y)
+    print "[INFO] Training result is as follows"
+    print cls.evaluate(X, y)
+    #              precision    recall  f1-score   support
+    # 
+    #         0.0       0.99      1.00      1.00      5000
+    #         1.0       1.00      0.74      0.85       122
+    # 
+    # avg / total       0.99      0.99      0.99      5122
+
+    #3. Save classifier
+    cls.dump(conf["classifier_path"])
+
+
+
+
