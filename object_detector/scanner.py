5d8afa19946e877775a87f988341bf9b67f35fa6
jeongjoonsup <penny4860@gmail.com>
2016-08-20 16:53:57 +0900

    [ImageScanner class] scale member added
    member var. to get current scale for the original
    image added

--- object_detector/scanner.py
+++ object_detector/scanner.py
@@ -37,6 +37,8 @@ class ImageScanner(object):
     
     def __init__(self, image):
         self._layer = image
+        self._original_dim = image.shape
+        self.scale_for_original = 1.0
     
     def get_next_patch(self, step_size=(10, 10), window_size=(30, 30)):
         for y in range(0, self._layer.shape[0] - window_size[0], step_size[0]):
@@ -56,8 +58,9 @@ class ImageScanner(object):
             min_w = min_size[1]
             if h < min_h or w < min_w:
                 break
+            self.scale_for_original = self.scale_for_original * scale
             yield self._layer
-    
+
     
 if __name__ == "__main__":
     import doctest
