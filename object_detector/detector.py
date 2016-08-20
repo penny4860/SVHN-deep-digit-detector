37872cc1f2720d5a762b9c296859328fef6515d6
jeongjoonsup <penny4860@gmail.com>
2016-08-20 19:37:47 +0900

    nms pyimagesearch implementation added

--- object_detector/detector.py
+++ object_detector/detector.py
@@ -36,6 +36,51 @@ class Detector(object):
     def hard_negative_mine(self):
         pass
     
+    def _do_nms(self, boxes, probs, overlapThresh=0.5):
+        if len(boxes) == 0:
+            return []
+     
+        boxes = np.array(boxes, dtype="float")
+        probs = np.array(probs)
+     
+        pick = []
+        y1 = boxes[:, 0]
+        y2 = boxes[:, 1]
+        x1 = boxes[:, 2]
+        x2 = boxes[:, 3]
+     
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(probs)
+        # keep looping while some indexes still remain in the indexes list
+        while len(idxs) > 0:
+            # grab the last index in the indexes list and add the index value to the list of
+            # picked indexes
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+    
+            # find the largest (x, y) coordinates for the start of the bounding box and the
+            # smallest (x, y) coordinates for the end of the bounding box
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+    
+            # compute the width and height of the bounding box
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+    
+            # compute the ratio of overlap
+            overlap = (w * h) / area[idxs[:last]]
+    
+            # delete all indexes from the index list that have overlap greater than the
+            # provided overlap threshold
+            idxs = np.delete(idxs, np.concatenate(([last],
+                np.where(overlap > overlapThresh)[0])))
+            
+        # return only the bounding boxes that were picked
+        return boxes[pick].astype("int"), probs[pick]
+    
 if __name__ == "__main__":
     import file_io
     import cv2
