66c4e3585804c9334a04e66c8d67609bf8481e54
jeongjoonsup <penny4860@gmail.com>
2016-08-20 14:15:32 +0900

    extract_features driver implementation

--- extract_features.py
+++ extract_features.py
@@ -1,96 +1,77 @@
-# USAGE
-# python extract_features.py --conf conf/cars.json
+#-*- coding: utf-8 -*-
 
-# import the necessary packages
-from __future__ import print_function
-from sklearn.feature_extraction.image import extract_patches_2d
-from pyimagesearch.object_detection import helpers
-from pyimagesearch.descriptors import HOG
-from pyimagesearch.utils import dataset
-from pyimagesearch.utils import Conf
-from imutils import paths
-from scipy import io
 import numpy as np
-import progressbar
-import argparse
 import random
 import cv2
+import object_detector.file_io as file_io
+import object_detector.descriptor as descriptor
+import object_detector.utils as utils
 
-# construct the argument parser and parse the command line arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--conf", required=True, help="path to the configuration file")
-args = vars(ap.parse_args())
+CONFIGURATION_FILE = "conf/cars.json"
+POSITIVE_LABEL_NUMBER = 1
+NEGATIVE_LABEL_NUMBER = 0
 
-# load the configuration file
-conf = Conf(args["conf"])
+conf = file_io.FileJson().read(CONFIGURATION_FILE)
 
 # initialize the HOG descriptor along with the list of data and labels
-hog = HOG(orientations=conf["orientations"], pixelsPerCell=tuple(conf["pixels_per_cell"]),
-    cellsPerBlock=tuple(conf["cells_per_block"]), normalize=conf["normalize"])
-data = []
-labels = []
+hog = descriptor.HOG(conf['orientations'],
+                     conf['pixels_per_cell'],
+                     conf['cells_per_block'])
 
-# grab the set of ground-truth images and select a percentage of them for training
-trnPaths = list(paths.list_images(conf["image_dataset"]))
-trnPaths = random.sample(trnPaths, int(len(trnPaths) * conf["percent_gt_images"]))
-print("[INFO] describing training ROIs...")
+train_image_files = file_io.list_files(conf["image_dataset"], "*.jpg")
+train_image_files = random.sample(train_image_files, int(len(train_image_files) * conf["percent_gt_images"]))
 
-# setup the progress bar
-widgets = ["Extracting: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
-pbar = progressbar.ProgressBar(maxval=len(trnPaths), widgets=widgets).start()
+negative_image_files = file_io.list_files(conf["image_distractions"], "*.jpg")
+negative_image_files = random.sample(negative_image_files, conf["num_distraction_images"])
 
-# loop over the training paths
-for (i, trnPath) in enumerate(trnPaths):
+positive_data = None
+negative_data = None
+
+for (i, file_) in enumerate(train_image_files):
     # load the image, convert it to grayscale, and extract the image ID from the path
-    image = cv2.imread(trnPath)
+    image = cv2.imread(file_)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    imageID = trnPath[trnPath.rfind("_") + 1:].replace(".jpg", "")
-
+    imageID = utils.get_file_id(file_)
+    
     # load the annotation file associated with the image and extract the bounding box
-    p = "{}/annotation_{}.mat".format(conf["image_annotations"], imageID)
-    bb = io.loadmat(p)["box_coord"][0]
-    roi = helpers.crop_ct101_bb(image, bb, padding=conf["offset"], dstSize=tuple(conf["window_dim"]))
-
-    # define the list of ROIs that will be described, based on whether or not the
-    # horizontal flip of the image should be used
+    annotation_file = "{}/annotation_{}.mat".format(conf["image_annotations"], imageID)
+    bb = file_io.FileMat().read(annotation_file)["box_coord"][0]
+    roi = utils.crop_bb(image, bb, padding=conf["offset"], dst_size=tuple(conf["window_dim"]))
+    
+    # Todo : augment modulization
     rois = (roi, cv2.flip(roi, 1)) if conf["use_flip"] else (roi,)
-
-    # loop over the ROIs
-    for roi in rois:
-        # extract features from the ROI and update the list of features and labels
-        features = hog.describe(roi)
-        data.append(features)
-        labels.append(1)
-
-    # update the progress bar
-    pbar.update(i)
-
-# grab the distraction image paths and reset the progress bar
-pbar.finish()
-dstPaths = list(paths.list_images(conf["image_distractions"]))
-pbar = progressbar.ProgressBar(maxval=conf["num_distraction_images"], widgets=widgets).start()
-print("[INFO] describing distraction ROIs...")
+    
+    features = hog.describe(rois)
+    
+    if positive_data is None:
+        positive_data = features
+    else:
+        positive_data = np.concatenate([positive_data, features], axis = 0)
 
 # loop over the desired number of distraction images
-for i in np.arange(0, conf["num_distraction_images"]):
-    # randomly select a distraction images, load it, convert it to grayscale, and
-    # then extract random pathces from the image
-    image = cv2.imread(random.choice(dstPaths))
+for (i, file_) in enumerate(negative_image_files):
+    image = cv2.imread(file_)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    patches = extract_patches_2d(image, tuple(conf["window_dim"]),
-        max_patches=conf["num_distractions_per_image"])
+    
+    patches = utils.crop_random(image, tuple(conf["window_dim"]), conf["num_distractions_per_image"])
+    features = hog.describe(patches)
+    
+    if negative_data is None:
+        negative_data = features
+    else:
+        negative_data = np.concatenate([negative_data, features], axis = 0)
 
-    # loop over the patches
-    for patch in patches:
-        # extract features from the patch, then update teh data and label list
-        features = hog.describe(patch)
-        data.append(features)
-        labels.append(-1)
+positive_labels = np.zeros((len(positive_data), 1)) + POSITIVE_LABEL_NUMBER
+negative_labels = np.zeros((len(negative_data), 1)) + NEGATIVE_LABEL_NUMBER
+pos_set = np.concatenate([positive_labels, positive_data], axis=1)
+neg_set = np.concatenate([negative_labels, negative_data], axis=1)
+data = np.concatenate([pos_set, neg_set], axis=0)
 
-    # update the progress bar
-    pbar.update(i)
+print("[INFO] number of positive data : {}, number of negative data : {}".format(positive_data.shape, negative_data.shape))
 
-# dump the dataset to file
-pbar.finish()
-print("[INFO] dumping features and labels to file...")
-dataset.dump_dataset(data, labels, conf["features_path"], "features")
+file_io.FileHDF5().write(data, conf["features_path"], "features")
+
+
+
+
+    
