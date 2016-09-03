5bb650d2f526a97f5cf1b7e51e86e0acf9a0fc87
jeongjoonsup <penny4860@gmail.com>
2016-09-03 21:15:10 +0900

    driver refactoring

--- extract_features.py
+++ extract_features.py
@@ -7,29 +7,33 @@ import object_detector.descriptor as descriptor
 if __name__ == "__main__":
     CONFIGURATION_FILE = "conf/cars.json"
     conf = file_io.FileJson().read(CONFIGURATION_FILE)
-    
+     
     # 1. Initialize Descriptor instance
     hog = descriptor.HOG(conf['orientations'],
                          conf['pixels_per_cell'],
                          conf['cells_per_block'])
-    
+     
     # 2. Initialize FeatureGetter instance
-    getter = descriptor.FeatureGetter(descriptor=hog, patch_size=conf['window_dim'])
-    
+    getter = file_io.FeatureGetter(descriptor=hog, patch_size=conf['window_dim'])
+     
     # 3. Get Feature sets
-    positive_dataset = getter.get_positive_sets(image_dir=conf["image_dataset"],
+    getter.add_positive_sets(image_dir=conf["image_dataset"],
                                                         pattern="*.jpg", 
                                                         annotation_path=conf['image_annotations'],
                                                         padding=conf['offset'])
-    negative_dataset = getter.get_negative_sets(image_files=conf["image_distractions"],
+    getter.add_negative_sets(image_dir=conf["image_distractions"],
                                                         pattern="*.jpg",
                                                         n_samples_per_img=5,
                                                         sample_ratio=0.5)
-    dataset = np.concatenate([positive_dataset, negative_dataset], axis=0)
-    print("[INFO] number of positive data : {}, number of negative data : {}".format(positive_dataset.shape, negative_dataset.shape))
-
+     
+    getter.summary()
+     
     # 4. Save dataset
-    file_io.FileHDF5().write(dataset, "", "features")
+    getter.save(config_file="feature_config.pkl", data_file="feature_data.hdf5")
+    del getter
+    
+    getter = file_io.FeatureGetter.load(config_file="feature_config.pkl", data_file="feature_data.hdf5")
+    getter.summary()
 
 
     
