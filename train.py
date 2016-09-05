3889f9c9be0f4145c313fb780594d6393ec64b4c
jeongjoonsup <penny4860@gmail.com>
2016-09-05 11:27:00 +0900

    train script is modified using FeatureGetter class

--- train.py
+++ train.py
@@ -4,25 +4,29 @@ import object_detector.file_io as file_io
 import object_detector.classifier as classifier
 import numpy as np
 
-CONFIGURATION_FILE = "conf/cars.json"
 HARD_NEGATIVE_OPTION = True
+C = 0.01
+CLS_PATH = "cls.pkl"
 
 if __name__ == "__main__":
     
-    conf = file_io.FileJson().read(CONFIGURATION_FILE)
-    
+
     #1. Load Features and Labels
-    # Todo : feature 와 label 을 나누어서 hdf file 에 쓰자.
-    data = file_io.FileHDF5().read(conf["features_path"], conf["db_name"])
-    if HARD_NEGATIVE_OPTION:
-        hard_negative_data = file_io.FileHDF5().read(conf["features_path"], "hard_negatives")
-        data = np.concatenate([data, hard_negative_data], axis=0)
+    getter = file_io.FeatureGetter.load(config_file="feature_config.pkl", data_file="feature_data.hdf5")
+    getter.summary()
+    data = np.array(getter.dataset)
+    
+#     # Todo : feature 와 label 을 나누어서 hdf file 에 쓰자.
+#     data = file_io.FileHDF5().read(conf["features_path"], conf["db_name"])
+#     if HARD_NEGATIVE_OPTION:
+#         hard_negative_data = file_io.FileHDF5().read(conf["features_path"], "hard_negatives")
+#         data = np.concatenate([data, hard_negative_data], axis=0)
 
     y = data[:, 0]
     X = data[:, 1:]
 
     #2. Train
-    cls = classifier.LinearSVM(C=conf["C"], random_state=111)
+    cls = classifier.LinearSVM(C=C, random_state=111)
     cls.train(X, y)
     print "[INFO] Training result is as follows"
     print cls.evaluate(X, y)
@@ -34,8 +38,7 @@ if __name__ == "__main__":
     # 
     # avg / total       1.00      1.00      1.00      5123
 
- 
     #3. Save classifier
-    cls.dump(conf["classifier_path"])
+    cls.dump(CLS_PATH)
 
 
