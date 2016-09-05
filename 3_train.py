ebefc668897c25d64fa025463b95ad8e66e6bd13
jeongjoonsup <penny4860@gmail.com>
2016-09-05 13:36:30 +0900

    train driver confirmed

--- 3_train.py
+++ 3_train.py
@@ -1,18 +1,25 @@
 #-*- coding: utf-8 -*-
 
 import object_detector.file_io as file_io
+
+import object_detector.descriptor as descriptor
+import object_detector.extractor as extractor
 import object_detector.classifier as classifier
 import numpy as np
 
 HARD_NEGATIVE_OPTION = True
-C = 0.01
-CLS_PATH = "cls.pkl"
+CONFIGURATION_FILE = "conf/new_format.json"
+PATCH_SIZE = (32, 96)
 
 if __name__ == "__main__":
+    conf = file_io.FileJson().read(CONFIGURATION_FILE)
     
-
     #1. Load Features and Labels
-    getter = file_io.FeatureGetter.load(config_file="feature_config.pkl", data_file="feature_data.hdf5")
+    hog = descriptor.HOG(conf["descriptor"]["parameters"]["orientations"],
+                     conf["descriptor"]["parameters"]["pixels_per_cell"],
+                     conf["descriptor"]["parameters"]["cells_per_block"])
+    
+    getter = extractor.FeatureExtractor.load(hog, PATCH_SIZE, data_file=conf["extractor"]["output_file"])
     getter.summary()
     data = np.array(getter.dataset)
     
@@ -26,7 +33,7 @@ if __name__ == "__main__":
     X = data[:, 1:]
 
     #2. Train
-    cls = classifier.LinearSVM(C=C, random_state=111)
+    cls = classifier.LinearSVM(C=conf["classifier"]["parameters"]["C"], random_state=111)
     cls.train(X, y)
     print "[INFO] Training result is as follows"
     print cls.evaluate(X, y)
@@ -39,6 +46,6 @@ if __name__ == "__main__":
     # avg / total       1.00      1.00      1.00      5123
 
     #3. Save classifier
-    cls.dump(CLS_PATH)
+    cls.dump(conf["classifier"]["output_file"])
 
 
