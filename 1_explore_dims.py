65b674adf53c0b168c2bbfc8cebc28e8129e3adc
jeongjoonsup <penny4860@gmail.com>
2016-09-05 11:48:38 +0900

    driver file rename

--- 1_explore_dims.py
+++ 1_explore_dims.py
@@ -5,7 +5,7 @@
 import numpy as np
 import object_detector.file_io as file_io
 
-CONFIGURATION_FILE = "conf/cars.json"
+CONFIGURATION_FILE = "conf/new_format.json"
 
 if __name__ == "__main__":
     conf_ = file_io.FileJson().read(CONFIGURATION_FILE)
@@ -13,16 +13,14 @@ if __name__ == "__main__":
     widths = []
     heights = []
     
-    files = file_io.list_files(conf_["image_annotations"], "*.mat")
+    files = file_io.list_files(conf_["annotations_dir"], "*.mat")
     for file_ in files:  
         (y, h, x, w) = file_io.FileMat().read(file_)["box_coord"][0]
         widths.append(w - x)
         heights.append(h - y)
       
-    # compute the average of both the width and height lists
     (avgWidth, avgHeight) = (np.mean(widths), np.mean(heights))
-    print("[INFO] avg. width: {:.2f}".format(avgWidth))
-    print("[INFO] avg. height: {:.2f}".format(avgHeight))
-    print("[INFO] aspect ratio: {:.2f}".format(avgWidth / avgHeight))
+    print "[INFO] avg. width: {:.2f}".format(avgWidth)
+    print "[INFO] avg. height: {:.2f}".format(avgHeight)
     
 
