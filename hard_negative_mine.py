282c3c457f6de5c1761333f1611f7bf28182c1c2
jeongjoonsup <penny4860@gmail.com>
2016-09-05 15:32:41 +0900

    hnm driver confirmed

--- hard_negative_mine.py
+++ hard_negative_mine.py
@@ -6,27 +6,43 @@ import object_detector.file_io as file_io
 import object_detector.descriptor as descriptor
 import object_detector.classifier as classifier
 import object_detector.detector as detector
+import object_detector.extractor as extractor
 
-CONFIGURATION_FILE = "conf/cars.json"
+
+CONFIGURATION_FILE = "conf/new_format.json"
+PATCH_SIZE = (32, 96)
 
 if __name__ == "__main__":
     conf = file_io.FileJson().read(CONFIGURATION_FILE)
-    hog = descriptor.HOG(conf['orientations'],
-                     conf['pixels_per_cell'],
-                     conf['cells_per_block'])
-    cls = classifier.LinearSVM.load(conf["classifier_path"])
-  
+    hog = descriptor.HOG(conf["descriptor"]["parameters"]["orientations"],
+                         conf["descriptor"]["parameters"]["pixels_per_cell"],
+                         conf["descriptor"]["parameters"]["cells_per_block"])
+    cls = classifier.LinearSVM.load(conf["classifier"]["output_file"])
+    
     detector = detector.Detector(hog, cls)
-    negative_image_files = file_io.list_files(conf["image_distractions"], "*.jpg", conf["hn_num_distraction_images"])
+    negative_image_files = file_io.list_files(conf["dataset"]["neg_data_dir"], 
+                                              conf["dataset"]["neg_format"], 
+                                              n_files_to_sample=conf["hard_negative_mine"]["n_images"])
+    
     features, probs = detector.hard_negative_mine(negative_image_files, 
-                                               conf["window_dim"], 
-                                               conf["window_step"], 
-                                               conf["pyramid_scale"], 
-                                               threshold_prob=0.5)
+                                               conf["detector"]["window_dim"], 
+                                               conf["hard_negative_mine"]["window_step"], 
+                                               conf["hard_negative_mine"]["pyramid_scale"], 
+                                               threshold_prob=conf["hard_negative_mine"]["min_probability"])
     
-    negative_labels = np.zeros((len(features), 1))
-    negative_set = np.concatenate([negative_labels, features], axis=1)
-    file_io.FileHDF5().write(negative_set, conf["features_path"], "hard_negatives", write_mode="a")
+    print "[HNM INFO] : number of mined negative patches {}".format(len(features))
+    print "[HNM INFO] : probabilities of mined negative patches {}".format(probs)
     
-    print len(negative_set)
-    print "done"
+    getter = extractor.FeatureExtractor.load(descriptor=hog, patch_size=PATCH_SIZE, data_file=conf["extractor"]["output_file"])
+    getter.summary()
+    getter.add_data(features, -1)
+    getter.summary()
+    
+    getter.save(data_file=conf["extractor"]["output_file"])
+
+    
+    
+    
+    
+    
+
