a02c2877247e42205f7feb001acb96f422c2a815
jeongjoonsup <penny4860@gmail.com>
2016-09-07 21:27:10 +0900

    mAP script : save tmp boxes

---
+++ mAP.py
@@ -0,0 +1,60 @@
+
+import object_detector.file_io as file_io
+import object_detector.factory as factory
+import cv2
+
+import pickle
+
+CONFIGURATION_FILE = "conf/new_format.json"
+
+if __name__ == "__main__":
+    
+    # 1. Load configuration file and test images
+    conf = file_io.FileJson().read(CONFIGURATION_FILE)
+    test_image_files = file_io.list_files(conf["dataset"]["pos_data_dir"], n_files_to_sample=2)
+
+    # 2. Build detector and save it   
+    detector = factory.Factory.create_detector(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"],
+                                               conf["classifier"]["algorithm"], conf["classifier"]["parameters"], conf["classifier"]["output_file"])
+    patches = []
+
+    # 3. Run detector on Test images 
+    for image_file in test_image_files:
+        test_image = cv2.imread(image_file)
+        test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+      
+        print "[INFO] Test Image shape: {0}".format(test_image.shape)
+
+        boxes, probs = detector.run(test_image, 
+                                    conf["detector"]["window_dim"], 
+                                    conf["detector"]["window_step"], 
+                                    conf["detector"]["pyramid_scale"], 
+                                    threshold_prob=0.0)
+        detector.show_boxes(test_image, boxes)
+        patches += boxes.tolist()
+    
+    # temporaty save boxes
+    with open("patches.pkl", 'wb') as f:
+        pickle.dump(patches, f)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+
+
+
+
