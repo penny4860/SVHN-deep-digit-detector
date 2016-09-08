ef072c0fe90bf68743fa2298356a2fc5cf06d331
jeongjoonsup <penny4860@gmail.com>
2016-09-08 20:47:07 +0900

    load test vector files

--- mAP.py
+++ mAP.py
@@ -61,60 +61,60 @@ if __name__ == "__main__":
     # 2. Build detector and save it   
     detector = factory.Factory.create_detector(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"],
                                                conf["classifier"]["algorithm"], conf["classifier"]["parameters"], conf["classifier"]["output_file"])
-    patches = []
-    probs = []
-    gts = []
-     
-    # 3. Run detector on Test images 
-    for image_file in test_image_files:
-        test_image = cv2.imread(image_file)
-        test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
-            
-        print "[INFO] Test Image shape: {0}".format(test_image.shape)
-      
-        boxes, probs_ = detector.run(test_image, 
-                                    conf["detector"]["window_dim"], 
-                                    conf["detector"]["window_step"], 
-                                    conf["detector"]["pyramid_scale"], 
-                                    threshold_prob=0.0)
-         
-        # Test Image �� ���� Ground-Truth �� Read
-        truth_bb = get_truth_bb(image_file, conf["dataset"]['annotations_dir'])
-        ious = calc_iou(boxes, truth_bb)
-        is_positive = ious > 0.5
-        detector.show_boxes(test_image, boxes)
-        
-        patches += boxes.tolist()
-        probs += probs_.tolist()
-        gts += is_positive.tolist()
-          
-    # temporaty save boxes
-    with open("patches.pkl", 'wb') as f:
-        pickle.dump(patches, f)
-    with open("probs.pkl", 'wb') as f:
-        pickle.dump(probs, f)
-    with open("gt.pkl", 'wb') as f:
-        pickle.dump(gts, f)
-        
-    print probs
-    print gts
-    
-#     # load boxes
-#     with open("patches.pkl", 'rb') as f:
-#         boxes = pickle.load(f)
-#     with open("probs.pkl", 'rb') as f:
-#         probs = pickle.load(f)
+#     patches = []
+#     probs = []
+#     gts = []
+#      
+#     # 3. Run detector on Test images 
+#     for image_file in test_image_files:
+#         test_image = cv2.imread(image_file)
+#         test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+#             
+#         print "[INFO] Test Image shape: {0}".format(test_image.shape)
+#       
+#         boxes, probs_ = detector.run(test_image, 
+#                                     conf["detector"]["window_dim"], 
+#                                     conf["detector"]["window_step"], 
+#                                     conf["detector"]["pyramid_scale"], 
+#                                     threshold_prob=0.0)
+#          
+#         # Test Image �� ���� Ground-Truth �� Read
+#         truth_bb = get_truth_bb(image_file, conf["dataset"]['annotations_dir'])
+#         ious = calc_iou(boxes, truth_bb)
+#         is_positive = ious > 0.5
+#         detector.show_boxes(test_image, boxes)
+#         
+#         patches += boxes.tolist()
+#         probs += probs_.tolist()
+#         gts += is_positive.tolist()
+#           
+#     # temporaty save boxes
+#     with open("patches.pkl", 'wb') as f:
+#         pickle.dump(patches, f)
+#     with open("probs.pkl", 'wb') as f:
+#         pickle.dump(probs, f)
 #     with open("gt.pkl", 'wb') as f:
-#         truth_bb = pickle.load(f)
-#  
-#     ious = calc_iou(boxes, truth_bb)
-#     print ious
+#         pickle.dump(gts, f)
+#         
+#     print probs
+#     print gts
+    
+    # load boxes
+    with open("patches.pkl", 'rb') as f:
+        patches = pickle.load(f)
+    with open("probs.pkl", 'rb') as f:
+        probs = pickle.load(f)
+    with open("gt.pkl", 'rb') as f:
+        gt = pickle.load(f)
 
+    patches = np.array(patches)
+    probs = np.array(probs)
+    gt = np.array(gt)
+    
+    print patches.shape, probs.shape, gt.shape
+    
 
     """
-    2. Ground-Truth Box
-    
-        
     3. Plot the precision-recall graph
     
     4. Calculate interpolated precision on n-recall values
