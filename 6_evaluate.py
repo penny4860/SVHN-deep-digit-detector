a473dd5c58ddd591cf6f4daf46b98d1a4943dce0
jeongjoonsup <penny4860@gmail.com>
2016-09-10 12:50:29 +0900

    nit

--- 6_evaluate.py
+++ 6_evaluate.py
@@ -11,21 +11,22 @@ if __name__ == "__main__":
     conf = file_io.FileJson().read(CONFIGURATION_FILE)
     #test_image_files = file_io.list_files(conf["dataset"]["pos_data_dir"], n_files_to_sample=2)
     test_image_files = file_io.list_files(conf["dataset"]["pos_data_dir"])
+    test_image_files = test_image_files[:10]
 
     # 2. Build detector and save it   
     detector = factory.Factory.create_detector(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"],
                                                conf["classifier"]["algorithm"], conf["classifier"]["parameters"], conf["classifier"]["output_file"])
-    
-    
-    print evaluate.Evaluator().eval_average_precision(test_image_files, 
+     
+     
+    ap = evaluate.Evaluator().eval_average_precision(test_image_files, 
                                conf["dataset"]['annotations_dir'], 
                                detector, 
                                conf["detector"]["window_dim"],
                                conf["detector"]["window_step"],
                                conf["detector"]["pyramid_scale"],
                                True)
-
-    #print calc_average_precision(recall_precision)
+    
+    print ap
     
 #     [INFO] Test Image shape: (197L, 300L)
 #     [INFO] Test Image shape: (197L, 300L)
