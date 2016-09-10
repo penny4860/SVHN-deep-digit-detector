35064d3ecff16dd5b258b8f04e893cd247527e6c
jeongjoonsup <penny4860@gmail.com>
2016-09-10 11:30:16 +0900

    refactoring on going

--- mAP.py
+++ mAP.py
@@ -50,7 +50,7 @@ def calc_precision_recall(probs, ground_truths):
     probs = np.array(probs)
     ground_truths = np.array(ground_truths)
     
-    dataset = np.concatenate([probs.reshape(-1,1), gt.reshape(-1,1)], axis=1)
+    dataset = np.concatenate([probs.reshape(-1,1), ground_truths.reshape(-1,1)], axis=1)
     dataset = dataset[dataset[:, 0].argsort()[::-1]]
     
     # print dataset
@@ -100,60 +100,37 @@ if __name__ == "__main__":
     # 2. Build detector and save it   
     detector = factory.Factory.create_detector(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"],
                                                conf["classifier"]["algorithm"], conf["classifier"]["parameters"], conf["classifier"]["output_file"])
-#     patches = []
-#     probs = []
-#     gts = []
-#      
-#     # 3. Run detector on Test images 
-#     for image_file in test_image_files:
-#         test_image = cv2.imread(image_file)
-#         test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
-#             
-#         print "[INFO] Test Image shape: {0}".format(test_image.shape)
-#       
-#         boxes, probs_ = detector.run(test_image, 
-#                                     conf["detector"]["window_dim"], 
-#                                     conf["detector"]["window_step"], 
-#                                     conf["detector"]["pyramid_scale"], 
-#                                     threshold_prob=0.0)
-#          
-#         # Test Image �� ���� Ground-Truth �� Read
-#         truth_bb = get_truth_bb(image_file, conf["dataset"]['annotations_dir'])
-#         ious = calc_iou(boxes, truth_bb)
-#         is_positive = ious > 0.5
-#         detector.show_boxes(test_image, boxes)
-#         
-#         patches += boxes.tolist()
-#         probs += probs_.tolist()
-#         gts += is_positive.tolist()
-#           
-#     # temporaty save boxes
-#     with open("patches.pkl", 'wb') as f:
-#         pickle.dump(patches, f)
-#     with open("probs.pkl", 'wb') as f:
-#         pickle.dump(probs, f)
-#     with open("gt.pkl", 'wb') as f:
-#         pickle.dump(gts, f)
-#         
-#     print probs
-#     print gts
-    
-    # load boxes
-    with open("patches.pkl", 'rb') as f:
-        patches = pickle.load(f)
-    with open("probs.pkl", 'rb') as f:
-        probs = pickle.load(f)
-    with open("gt.pkl", 'rb') as f:
-        gt = pickle.load(f)
-
-    probs.append(0.8)
-    gt.append(0)
-    
+    patches = []
+    probs = []
+    gts = []
+      
+    # 3. Run detector on Test images 
+    for image_file in test_image_files:
+        test_image = cv2.imread(image_file)
+        test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+             
+        print "[INFO] Test Image shape: {0}".format(test_image.shape)
+       
+        boxes, probs_ = detector.run(test_image, 
+                                    conf["detector"]["window_dim"], 
+                                    conf["detector"]["window_step"], 
+                                    conf["detector"]["pyramid_scale"], 
+                                    threshold_prob=0.0)
+          
+        # Test Image �� ���� Ground-Truth �� Read
+        truth_bb = get_truth_bb(image_file, conf["dataset"]['annotations_dir'])
+        ious = calc_iou(boxes, truth_bb)
+        is_positive = ious > 0.5
+        # detector.show_boxes(test_image, boxes)
+         
+        patches += boxes.tolist()
+        probs += probs_.tolist()
+        gts += is_positive.tolist()
+           
     probs = np.array(probs)
-    gt = np.array(gt)
+    gts = np.array(gts)
 
-    recall_precision = calc_precision_recall(probs, gt)
-    
+    recall_precision = calc_precision_recall(probs, gts)
     print calc_average_precision(recall_precision)
     
 
