2a9d5143d8a40b8bdc15a40e1b62d93187069e79
jeongjoonsup <penny4860@gmail.com>
2016-09-10 14:39:39 +0900

    client code edited

--- 6_evaluate.py
+++ 6_evaluate.py
@@ -9,40 +9,25 @@ if __name__ == "__main__":
     
     # 1. Load configuration file and test images
     conf = file_io.FileJson().read(CONFIGURATION_FILE)
-    #test_image_files = file_io.list_files(conf["dataset"]["pos_data_dir"], n_files_to_sample=2)
     test_image_files = file_io.list_files(conf["dataset"]["pos_data_dir"])
-    test_image_files = test_image_files[:10]
+    test_image_files = test_image_files[:20]
 
-    # 2. Build detector and save it   
+    # 2. Build detector
     detector = factory.Factory.create_detector(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"],
                                                conf["classifier"]["algorithm"], conf["classifier"]["parameters"], conf["classifier"]["output_file"])
-     
-     
-    ap = evaluate.Evaluator().eval_average_precision(test_image_files, 
+
+    # 3. Evaluate average precision     
+    evaluator = evaluate.Evaluator()
+    ap = evaluator.eval_average_precision(test_image_files, 
                                conf["dataset"]['annotations_dir'], 
                                detector, 
                                conf["detector"]["window_dim"],
                                conf["detector"]["window_step"],
-                               conf["detector"]["pyramid_scale"],
-                               True)
-    
+                               conf["detector"]["pyramid_scale"])
     print ap
-    
-#     [INFO] Test Image shape: (197L, 300L)
-#     [INFO] Test Image shape: (197L, 300L)
-#     0.848484848485
 
-    
-
-    
-    
-    
-
-    
-    
-    
-    
-    
+    evaluator.plot_recall_precision()
+    print evaluator._dataset
     
     
     
