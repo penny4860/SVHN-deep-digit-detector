76376d2edaa0e9a79aec281e30f761f6a2d981e9
jeongjoonsup <penny4860@gmail.com>
2016-09-14 13:19:56 +0900

    driver 3 confirmed

--- 3_train.py
+++ 3_train.py
@@ -2,40 +2,43 @@
 
 import object_detector.file_io as file_io
 import object_detector.factory as factory
+import argparse as ap
 
-HARD_NEGATIVE_OPTION = True
+DEFAULT_HNM_OPTION = True
 DEFAULT_CONFIG_FILE = "conf/car_side.json"
-PATCH_SIZE = (32, 96)
+DEFAULT_PATCH_SIZE = (32, 96)
 
 if __name__ == "__main__":
-    conf = file_io.FileJson().read(DEFAULT_CONFIG_FILE)
+    
+    parser = ap.ArgumentParser()
+    parser.add_argument('-c', "--config", help="Configuration File", default=DEFAULT_CONFIG_FILE)
+    parser.add_argument('-p', "--patch_size", help="Patch Size", default=DEFAULT_PATCH_SIZE)
+    parser.add_argument('-i', "--include_hnm", help="Include Hard Negative Mined Set", default=DEFAULT_HNM_OPTION)
+    
+    args = vars(parser.parse_args())
+    
+    conf = file_io.FileJson().read(args["config"])
     
     #1. Load Features and Labels
-    getter = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"], PATCH_SIZE, conf["extractor"]["output_file"])
+    getter = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], 
+                                              conf["descriptor"]["parameters"], 
+                                              args["patch_size"], 
+                                              conf["extractor"]["output_file"])
     getter.summary()
-    data = getter.get_dataset(include_hard_negative=HARD_NEGATIVE_OPTION)
+    data = getter.get_dataset(include_hard_negative=args["include_hnm"])
     
     y = data[:, 0]
     X = data[:, 1:]
  
     #2. Load classifier and Train
-    cls = factory.Factory.create_classifier(conf["classifier"]["algorithm"], conf["classifier"]["parameters"])
+    cls = factory.Factory.create_classifier(conf["classifier"]["algorithm"], 
+                                            conf["classifier"]["parameters"])
     
     cls.train(X, y)
     print "[INFO] Training result is as follows"
     print cls.evaluate(X, y)
-#     [FeatureGetter INFO] Positive samples: 246, Negative samples: 1925
-#     [FeatureGetter INFO] Feature Dimension: 5796
-#     [INFO] Training result is as follows
-#                  precision    recall  f1-score   support
-#     
-#             0.0       1.00      1.00      1.00      1925
-#             1.0       1.00      0.98      0.99       246
-#     
-#     avg / total       1.00      1.00      1.00      2171
- 
  
     #3. Save classifier
-    cls.dump(conf["classifier"]["output_file"])
+    #cls.dump(conf["classifier"]["output_file"])
 
 
