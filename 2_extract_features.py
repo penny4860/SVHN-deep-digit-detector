5c19c419198ef558ebcf950210c4a9d844de3274
jeongjoonsup <penny4860@gmail.com>
2016-09-14 12:50:09 +0900

    commented

--- 2_extract_features.py
+++ 2_extract_features.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     conf = file_io.FileJson().read(DEFAULT_CONFIG_FILE)
      
     # 1. Build FeatureExtrator instance
-    getter = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"], PATCH_SIZE)
+    extractor = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"], PATCH_SIZE)
       
     # 2. Get Feature sets
-    getter.add_positive_sets(image_dir=conf["dataset"]["pos_data_dir"],
+    extractor.add_positive_sets(image_dir=conf["dataset"]["pos_data_dir"],
                              pattern=conf["dataset"]["pos_format"], 
                              annotation_path=conf["dataset"]['annotations_dir'],
                              sample_ratio=conf["extractor"]["sampling_ratio_for_positive_images"],
@@ -21,20 +21,20 @@ if __name__ == "__main__":
                              )
      
     # Todo : positive sample 숫자에 따라 negative sample 숫자를 자동으로 정할 수 있도록 설정
-    getter.add_negative_sets(image_dir=conf["dataset"]["neg_data_dir"],
+    extractor.add_negative_sets(image_dir=conf["dataset"]["neg_data_dir"],
                              pattern=conf["dataset"]["neg_format"],
                              n_samples_per_img=conf["extractor"]["num_patches_per_negative_image"],
                              sample_ratio=conf["extractor"]["sampling_ratio_for_negative_images"])
       
-    getter.summary()
+    extractor.summary()
       
     # 3. Save dataset
-    getter.save(data_file=conf["extractor"]["output_file"])
-    del getter
-      
-    # 4. Test Loading dataset
-    getter = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"], PATCH_SIZE, conf["extractor"]["output_file"])
-    getter.summary()
+    extractor.save(data_file=conf["extractor"]["output_file"])
+#     del extractor
+#       
+#     # 4. Test Loading dataset
+#     extractor = factory.Factory.create_extractor(conf["descriptor"]["algorithm"], conf["descriptor"]["parameters"], PATCH_SIZE, conf["extractor"]["output_file"])
+#     extractor.summary()
  
  
      
