5a2110556961f4e88cc32b346d0cd26ec6e753e1
jeongjoonsup <penny4860@gmail.com>
2016-09-26 20:37:57 +0900

    calc_average_patch_size() replaced to svhn

--- object_detector/extractor.py
+++ object_detector/extractor.py
@@ -98,21 +98,59 @@ class FeatureExtractor():
         image_files = random.sample(image_files, int(len(image_files) * sample_ratio))
         return image_files
 
-# Todo : FeatureExtractor class member function
-def calc_average_patch_size(annotation_directory, pattern):
+
+import os
+def calc_average_patch_size(annotation_file):
     widths = []
     heights = []
     
-    files = file_io.list_files(annotation_directory, pattern)
-    for file_ in files:  
-        (y, h, x, w) = file_io.FileMat().read(file_)["box_coord"][0]
-        widths.append(w - x)
-        heights.append(h - y)
+    annotations = file_io.FileJson().read(annotation_file)
+    """    
+    {
+        u'boxes': 
+            [
+                {u'height': 219.0, u'width': 81.0, u'top': 77.0, u'left': 246.0, u'label': 1.0}, 
+                {u'height': 219.0, u'width': 96.0, u'top': 81.0, u'left': 323.0, u'label': 9.0}
+            ], 
+        u'filename': u'1.png'
+    }
+    """
+    image_path = os.path.split(annotation_file)[0]
+    
+    for annotation in annotations:
+        image = cv2.imread(os.path.join(image_path, annotation["filename"]))
+
+        for box in annotation["boxes"]:
+            x1 = int(box["left"])
+            y1 = int(box["top"])
+            w = int(box["width"])
+            h = int(box["height"])
+            label = int(box["label"])
+            cv2.rectangle(image, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
+            cv2.putText(image, str(label), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+
+            widths.append(w)
+            heights.append(h)
 
     height = np.mean(heights)
     width = np.mean(widths)
+    
+    print "h_min", np.min(heights)
+    print "h_max", np.max(heights)
+    print "w_min", np.min(widths)
+    print "w_max", np.max(widths)
+    
+    data = np.array([(h, w, h+w) for h, w in zip(heights, widths)])
+    print data.shape
+    
+    data = data[data[:, 0].argsort()]
+    print data[:20]
+
+    data = data[data[:, 1].argsort()]
+    print data[:20]
+
+    data = data[data[:, 2].argsort()]
+    print data[:20]
     return height, width
-
-
 
 
