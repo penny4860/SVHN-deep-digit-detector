fd062e921cb3b64dd446a733a29a5839d0fe322a
jeongjoonsup <penny4860@gmail.com>
2016-11-19 21:42:13 +0900

    image 당 2개 이상의 truth 에 대해서 AP-test 성공

--- object_detector/evaluate.py
+++ object_detector/evaluate.py
@@ -49,7 +49,6 @@ class Evaluator(object):
         --------
         """
         
-        patches = []
         probs = []
         gts = []
         
@@ -72,12 +71,11 @@ class Evaluator(object):
             ious = self._calc_iou(boxes, truth_bbs)
             is_positive = ious > 0.5
             
-            # IOU 가 50% 이상인 것 중에서 최대확률을 갖는 bounding-box 1개만 truth 로 간주한다.
+            # IOU 가 50% 이상인 것 중에서 최대확률을 갖는 bounding-box n_truth 개만 truth 로 간주한다.
             idxes = np.where(is_positive == 1)
             is_positive = np.zeros_like(is_positive)
-            is_positive[idxes[0][0]] = 1
+            is_positive[idxes[0][0:len(truth_bbs)]] = 1
              
-            patches += boxes.tolist()
             probs += probs_.tolist()
             gts += is_positive.tolist()
             
@@ -165,33 +163,39 @@ class Evaluator(object):
         inter_precision = max(inter_precision)
         return inter_precision
     
-    def _calc_iou(self, boxes, truth_box):
+    def _calc_iou(self, boxes, truth_boxes):
         
-        truth_box = truth_box[0]
+        ious_for_each_gt = []
         
-        y1 = boxes[:, 0]
-        y2 = boxes[:, 1]
-        x1 = boxes[:, 2]
-        x2 = boxes[:, 3]
+        for truth_box in truth_boxes:
+            y1 = boxes[:, 0]
+            y2 = boxes[:, 1]
+            x1 = boxes[:, 2]
+            x2 = boxes[:, 3]
+            
+            y1_gt = truth_box[0]
+            y2_gt = truth_box[1]
+            x1_gt = truth_box[2]
+            x2_gt = truth_box[3]
+            
+            xx1 = np.maximum(x1, x1_gt)
+            yy1 = np.maximum(y1, y1_gt)
+            xx2 = np.minimum(x2, x2_gt)
+            yy2 = np.minimum(y2, y2_gt)
         
-        y1_gt = truth_box[0]
-        y2_gt = truth_box[1]
-        x1_gt = truth_box[2]
-        x2_gt = truth_box[3]
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+            
+            intersections = w*h
+            As = (x2 - x1 + 1) * (y2 - y1 + 1)
+            B = (x2_gt - x1_gt + 1) * (y2_gt - y1_gt + 1)
+            
+            ious = intersections.astype(float) / (As + B -intersections)
+            ious_for_each_gt.append(ious)
         
-        xx1 = np.maximum(x1, x1_gt)
-        yy1 = np.maximum(y1, y1_gt)
-        xx2 = np.minimum(x2, x2_gt)
-        yy2 = np.minimum(y2, y2_gt)
-    
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
-        
-        intersections = w*h
-        As = (x2 - x1 + 1) * (y2 - y1 + 1)
-        B = (x2_gt - x1_gt + 1) * (y2_gt - y1_gt + 1)
-        
-        ious = intersections.astype(float) / (As + B -intersections)
+        # (n_truth, n_boxes)
+        ious = np.array(ious_for_each_gt)
+        ious = np.max(ious, axis = 0)
         return ious
 
 
