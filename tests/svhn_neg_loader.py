c66ef8b20e71b51f3242c072719e37ed4eaec171
jeongjoonsup <penny4860@gmail.com>
2016-12-20 16:19:44 +0900

    negative image loader added

--- tests/svhn_neg_loader.py
+++ tests/svhn_neg_loader.py
@@ -1,19 +1,31 @@
+#-*- coding: utf-8 -*-
+
 import numpy as np
 import scipy.io as io
 import cv2
+import utils
 
-files = ['../../datasets/svhn/train_32x32.mat']
+files = utils.list_files('../../datasets/svhn/train/negative_images', pattern="*.png", n_files_to_sample=5000, recursive_option=False)
 
-dataset = io.loadmat(files[0])
+print len(files)
 
-images = dataset['X']
-labels = dataset['y']
+patch_size = [32, 32]
+n_samples_per_img = 10
 
-images = np.transpose(images, (3, 0, 1, 2))
+negative_images = []
 
-print images.shape, labels.shape
+for image_file in files:
+    image = cv2.imread(image_file)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    if image.shape[0] >= patch_size[0] and image.shape[1] >= patch_size[0]:
+        patches = utils.crop_random(image, patch_size, n_samples_per_img)
+        negative_images.append(patches)
+    
+negative_images = np.concatenate(negative_images, axis=0)
+print negative_images.shape
+# (39350, 32, 32)
 
-cv2.imshow("", images[0])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imshow("", negative_images[-2])
+cv2.waitKey()
 
