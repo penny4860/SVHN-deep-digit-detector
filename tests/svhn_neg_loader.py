3a15b4bf2db29937421b9d99afb0779a62b928e9
jeongjoonsup <penny4860@gmail.com>
2016-12-20 15:41:04 +0900

    negative data loader is working

---
+++ tests/svhn_neg_loader.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.io as io
+import cv2
+
+files = ['../../datasets/svhn/train_32x32.mat']
+
+dataset = io.loadmat(files[0])
+
+images = dataset['X']
+labels = dataset['y']
+
+images = np.transpose(images, (3, 0, 1, 2))
+
+print images.shape, labels.shape
+
+cv2.imshow("", images[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
