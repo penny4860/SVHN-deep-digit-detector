e462928a6b1a8235b289dae2e0dee1509301a1f0
joonsup <joonsup@joonsup-PC>
2016-12-29 02:28:31 +0900

    client code 변경

--- 5_resizing_regions.py
+++ 5_resizing_regions.py
@@ -14,27 +14,6 @@ import numpy as np
 
 import digit_detector.region_proposal as rp
 import digit_detector.show as show
-import digit_detector.utils as utils
-
-def propose_patches(image, dst_size=(32, 32)):
-    detector = rp.MserDetector()
-    candidates_bbs = detector.detect(img, False)
-
-    patches = []
-    for bb in candidates_bbs:
-        y1, y2, x1, x2 = bb
-        width = x2 - x1 + 1
-        height = y2 - y1 + 1
-        
-        if width >= height:
-            pad_y = 0
-            pad_x = 0
-        else:
-            pad_x = int((height-width)/2)
-            pad_y = 0
-        sample = utils.crop_bb(img, bb, pad_size=(pad_y ,pad_x), dst_size=(32,32))
-        patches.append(sample)
-    return np.array(patches)
 
 # 1. image files
 img_file = img_files[0]
@@ -44,7 +23,7 @@ img = cv2.imread(img_file)
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 3. region proposals (N, 32, 32, 3)
-patches = propose_patches(img)
+patches = rp.propose_patches(img)
 show.plot_images(patches)
 
 # 4. Convert to gray
