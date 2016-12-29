1d9e0c7d58b50a523b4509cdea9980e2d7e5ae1a
jeongjoonsup <penny4860@gmail.com>
2016-12-29 15:05:04 +0900

    _do_nms() added

--- digit_detector/detect.py
+++ digit_detector/detect.py
@@ -26,15 +26,65 @@ def detect(image, model_filename, mean_value, input_shape = (32,32,1), threshold
 #     show.plot_images(temp, probs.tolist())
 #     show.plot_bounding_boxes(image, bbs, probs.tolist())
 
+    # Thresholding
     bbs = bbs[probs > threshold]
     probs = probs[probs > threshold]
-    
+
+    # Non Maximum Suppression
+    bbs, probs = _do_nms(bbs, probs, 0.3)
+
     for i, bb in enumerate(bbs):
         image = show.draw_box(image, bb, 2)
-            
     cv2.imshow("MSER + CNN", image)
     cv2.waitKey(0)
 
+# todo: code review
+def _do_nms(boxes, probs, overlapThresh=0.3):
+    """
+    Reference: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+    """
+    if len(boxes) == 0:
+        return []
+ 
+    boxes = np.array(boxes, dtype="float")
+    probs = np.array(probs)
+ 
+    pick = []
+    y1 = boxes[:, 0]
+    y2 = boxes[:, 1]
+    x1 = boxes[:, 2]
+    x2 = boxes[:, 3]
+ 
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(probs)
+    # keep looping while some indexes still remain in the indexes list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the index value to the list of
+        # picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of the bounding box and the
+        # smallest (x, y) coordinates for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have overlap greater than the
+        # provided overlap threshold
+        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlapThresh)[0])))
+        
+    # return only the bounding boxes that were picked
+    return boxes[pick].astype("int"), probs[pick]
 
 if __name__ == "__main__":
     pass
