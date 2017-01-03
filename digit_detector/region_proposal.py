24c7b6ec123b61f69933e7b08c8c63162c3b6785
jeongjoonsup <penny4860@gmail.com>
2017-01-03 20:57:39 +0900

    nit : comment

--- digit_detector/region_proposal.py
+++ digit_detector/region_proposal.py
@@ -1,4 +1,4 @@
-
+#-*- coding: utf-8 -*-
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -8,6 +8,8 @@ import utils
 
 
 class Regions:
+    
+    """ Image 에서의 bounding box 를 관리하는 class """
     
     def __init__(self, image, boxes):
         self._image = image
