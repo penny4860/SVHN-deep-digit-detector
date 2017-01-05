6fb4d2ecd091eff0572f9266a2db7004bef856c7
jeongjoonsup <penny4860@gmail.com>
2017-01-05 21:38:31 +0900

    gather_samples() 모듈화

--- 1_sample_loader.py
+++ 1_sample_loader.py
@@ -22,73 +22,70 @@ NEG_OVERLAP_THD = 0.05
 POS_OVERLAP_THD = 0.6
 PATCH_SIZE = (32,32)
 
-# 1. file 을 load
-files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=N_IMAGES, random_order=False)
-annotation_file = "../datasets/svhn/train/digitStruct.json"
-detector = rp.MserRegionProposer()                      # todo : interface 에 의존하도록 수정하자.
-annotator = ann.SvhnAnnotation(annotation_file)  # todo : interface 에 의존하도록 수정하자.
-iou_calculator = rp.IouCalculator()
+def gather_samples(image_files):
+    detector = rp.MserRegionProposer()                      # todo : interface 에 의존하도록 수정하자.
+    annotator = ann.SvhnAnnotation(annotation_file)  # todo : interface 에 의존하도록 수정하자.
+    iou_calculator = rp.IouCalculator()
+    
+    negative_samples = []
+    positive_samples = []
+    positive_labels = []
+    
+    bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ', progressbar.Bar(), ' (', progressbar.ETA(), ') ',], maxval=len(image_files)).start()
 
-negative_samples = []
-positive_samples = []
-positive_labels = []
-
-bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ', progressbar.Bar(), ' (', progressbar.ETA(), ') ',], maxval=len(files)).start()
-
-for i, image_file in enumerate(files):
-    print image_file,
-    image = cv2.imread(image_file)
- 
-    candidate_regions = detector.detect(image)
-    patches = candidate_regions.get_patches(dst_size=PATCH_SIZE)
+    for i, image_file in enumerate(image_files):
+        print image_file,
+        image = cv2.imread(image_file)
      
-    true_boxes, labels = annotator.get_boxes_and_labels(image_file)
-    truth_regions = rp.Regions(image, true_boxes)
-    truth_patches = truth_regions.get_patches(dst_size=PATCH_SIZE)
- 
-    ious, ious_max = iou_calculator.calc(candidate_regions.get_boxes(), truth_regions.get_boxes())
-#     show.plot_bounding_boxes(image, candidate_regions.get_boxes())
-#     show.plot_bounding_boxes(image, candidate_regions.get_boxes()[overlaps<OVERLAP_THD]) #negative sample plot
-   
-    # Ground Truth 와의 overlap 이 5% 미만인 모든 sample 을 negative set 으로 저장
-    negative_samples.append(candidate_regions.get_patches(PATCH_SIZE)[ious_max<NEG_OVERLAP_THD])
-     
-    for i, label in enumerate(labels):
-        samples = candidate_regions.get_patches(PATCH_SIZE)[ious[i,:]>POS_OVERLAP_THD]
-        labels_ = np.zeros((len(samples), )) + label
-        positive_samples.append(samples)
-        positive_labels.append(labels_)
+        candidate_regions = detector.detect(image)
+        patches = candidate_regions.get_patches(dst_size=PATCH_SIZE)
          
-    positive_samples.append(truth_regions.get_patches(PATCH_SIZE))
-    positive_labels.append(labels)
- 
+        true_boxes, labels = annotator.get_boxes_and_labels(image_file)
+        truth_regions = rp.Regions(image, true_boxes)
+        truth_patches = truth_regions.get_patches(dst_size=PATCH_SIZE)
      
-    bar.update(i)
- 
-bar.finish()
- 
- 
-negative_samples = np.concatenate(negative_samples, axis=0)    
-negative_labels = np.zeros((len(negative_samples), 1))
-positive_samples = np.concatenate(positive_samples, axis=0)    
-positive_labels = np.concatenate(positive_labels, axis=0)
- 
- 
-print negative_samples.shape, positive_samples.shape
-print negative_labels.shape, positive_labels.shape
- 
-show.plot_images(positive_samples, positive_labels.tolist())
-show.plot_images(negative_samples)
- 
-  
-# file_io.FileHDF5().write(negative_samples, "negative_images.hdf5", "images", "w", dtype="uint8")
-# file_io.FileHDF5().write(labels, "negative_images.hdf5", "labels", "a", dtype="int")
- 
-# show.plot_images(negative_samples)
+        ious, ious_max = iou_calculator.calc(candidate_regions.get_boxes(), truth_regions.get_boxes())
+       
+        # Ground Truth 와의 overlap 이 5% 미만인 모든 sample 을 negative set 으로 저장
+        negative_samples.append(candidate_regions.get_patches(PATCH_SIZE)[ious_max<NEG_OVERLAP_THD])
+         
+        for i, label in enumerate(labels):
+            samples = candidate_regions.get_patches(PATCH_SIZE)[ious[i,:]>POS_OVERLAP_THD]
+            labels_ = np.zeros((len(samples), )) + label
+            positive_samples.append(samples)
+            positive_labels.append(labels_)
+             
+        positive_samples.append(truth_regions.get_patches(PATCH_SIZE))
+        positive_labels.append(labels)
+        bar.update(i)
+    bar.finish()
+     
+    negative_samples = np.concatenate(negative_samples, axis=0)    
+    negative_labels = np.zeros((len(negative_samples), 1))
+    positive_samples = np.concatenate(positive_samples, axis=0)    
+    positive_labels = np.concatenate(positive_labels, axis=0)
     
-    
-    
+    return positive_samples, positive_labels, negative_samples, negative_labels
 
+
+if __name__ == "__main__":
+
+    # 1. file 을 load
+    files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=N_IMAGES, random_order=False)
+    annotation_file = "../datasets/svhn/train/digitStruct.json"
+    
+    positive_samples, positive_labels, negative_samples, negative_labels = gather_samples(files)
+         
+    print negative_samples.shape, positive_samples.shape
+    print negative_labels.shape, positive_labels.shape
+     
+    show.plot_images(positive_samples, positive_labels.tolist())
+    show.plot_images(negative_samples)
+     
+      
+    # file_io.FileHDF5().write(negative_samples, "negative_images.hdf5", "images", "w", dtype="uint8")
+    # file_io.FileHDF5().write(labels, "negative_images.hdf5", "labels", "a", dtype="int")
+     
 
 
 
