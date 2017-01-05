b219b700a166699e9300c07293a140ce0427d68e
jeongjoonsup <penny4860@gmail.com>
2017-01-05 22:30:58 +0900

    Extractor class 로 변경

--- 1_sample_loader.py
+++ 1_sample_loader.py
@@ -22,49 +22,62 @@ NEG_OVERLAP_THD = 0.05
 POS_OVERLAP_THD = 0.6
 PATCH_SIZE = (32,32)
 
-def gather_samples(image_files, patch_size, positive_overlap_thd, negative_overlap_thd):
-    detector = rp.MserRegionProposer()                      # todo : interface 에 의존하도록 수정하자.
-    annotator = ann.SvhnAnnotation(annotation_file)  # todo : interface 에 의존하도록 수정하자.
-    
-    negative_samples = []
-    positive_samples = []
-    positive_labels = []
-    
-    bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ', progressbar.Bar(), ' (', progressbar.ETA(), ') ',], maxval=len(image_files)).start()
 
-    for i, image_file in enumerate(image_files):
-        print image_file,
-        image = cv2.imread(image_file)
-     
-        candidate_regions = detector.detect(image)
-        patches = candidate_regions.get_patches(dst_size=PATCH_SIZE)
-         
-        true_boxes, labels = annotator.get_boxes_and_labels(image_file)
-        truth_regions = rp.Regions(image, true_boxes)
-        truth_patches = truth_regions.get_patches(dst_size=PATCH_SIZE)
-     
-        ious, ious_max = rp.calc_overlap(candidate_regions.get_boxes(), truth_regions.get_boxes())
-       
-        # Ground Truth 와의 overlap 이 5% 미만인 모든 sample 을 negative set 으로 저장
-        negative_samples.append(candidate_regions.get_patches(patch_size)[ious_max<negative_overlap_thd])
-         
-        for i, label in enumerate(labels):
-            samples = candidate_regions.get_patches(PATCH_SIZE)[ious[i,:]>positive_overlap_thd]
-            labels_ = np.zeros((len(samples), )) + label
-            positive_samples.append(samples)
-            positive_labels.append(labels_)
-             
-        positive_samples.append(truth_regions.get_patches(PATCH_SIZE))
-        positive_labels.append(labels)
-        bar.update(i)
-    bar.finish()
-     
-    negative_samples = np.concatenate(negative_samples, axis=0)    
-    negative_labels = np.zeros((len(negative_samples), 1))
-    positive_samples = np.concatenate(positive_samples, axis=0)    
-    positive_labels = np.concatenate(positive_labels, axis=0)
+class Extractor:
     
-    return positive_samples, positive_labels, negative_samples, negative_labels
+    def __init__(self):
+        pass
+    
+    def extract_patch(self, image_files, patch_size, positive_overlap_thd, negative_overlap_thd):
+        detector = rp.MserRegionProposer()                      # todo : interface 에 의존하도록 수정하자.
+        annotator = ann.SvhnAnnotation(annotation_file)  # todo : interface 에 의존하도록 수정하자.
+        
+        negative_samples = []
+        positive_samples = []
+        positive_labels = []
+        
+        bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ', progressbar.Bar(), ' (', progressbar.ETA(), ') ',], maxval=len(image_files)).start()
+    
+        for i, image_file in enumerate(image_files):
+            print image_file,
+            image = cv2.imread(image_file)
+         
+            candidate_regions = detector.detect(image)
+            patches = candidate_regions.get_patches(dst_size=PATCH_SIZE)
+             
+            true_boxes, labels = annotator.get_boxes_and_labels(image_file)
+            truth_regions = rp.Regions(image, true_boxes)
+            truth_patches = truth_regions.get_patches(dst_size=PATCH_SIZE)
+         
+            ious, ious_max = rp.calc_overlap(candidate_regions.get_boxes(), truth_regions.get_boxes())
+           
+            # Ground Truth 와의 overlap 이 5% 미만인 모든 sample 을 negative set 으로 저장
+            negative_samples.append(candidate_regions.get_patches(patch_size)[ious_max<negative_overlap_thd])
+             
+            for i, label in enumerate(labels):
+                samples = candidate_regions.get_patches(PATCH_SIZE)[ious[i,:]>positive_overlap_thd]
+                labels_ = np.zeros((len(samples), )) + label
+                positive_samples.append(samples)
+                positive_labels.append(labels_)
+                 
+            positive_samples.append(truth_regions.get_patches(PATCH_SIZE))
+            positive_labels.append(labels)
+            bar.update(i)
+        bar.finish()
+         
+        negative_samples = np.concatenate(negative_samples, axis=0)    
+        negative_labels = np.zeros((len(negative_samples), 1))
+        positive_samples = np.concatenate(positive_samples, axis=0)    
+        positive_labels = np.concatenate(positive_labels, axis=0)
+        
+        return positive_samples, positive_labels, negative_samples, negative_labels
+    
+    def _select_positive_patch(self):
+        pass
+    
+    def _select_negative_patch(self):
+        pass
+
 
 
 if __name__ == "__main__":
@@ -73,7 +86,7 @@ if __name__ == "__main__":
     files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=N_IMAGES, random_order=False)
     annotation_file = "../datasets/svhn/train/digitStruct.json"
     
-    positive_samples, positive_labels, negative_samples, negative_labels = gather_samples(files, PATCH_SIZE, POS_OVERLAP_THD, NEG_OVERLAP_THD)
+    positive_samples, positive_labels, negative_samples, negative_labels = Extractor().extract_patch(files, PATCH_SIZE, POS_OVERLAP_THD, NEG_OVERLAP_THD)
          
     print negative_samples.shape, positive_samples.shape
     print negative_labels.shape, positive_labels.shape
