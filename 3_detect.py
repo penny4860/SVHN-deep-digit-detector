04ea2d49e85fcf17fad8ed4ef61ef08acffdc018
jeongjoonsup <penny4860@gmail.com>
2017-01-07 03:19:10 +0900

    detect driver 정리

--- 3_detect.py
+++ 3_detect.py
@@ -11,18 +11,21 @@ import digit_detector.file_io as file_io
 
 model_filename = "detector_model.hdf5"
 mean_value = 107.524
+model_input_shape = (32,32,1)
 DIR = '../datasets/svhn/extra'
+
 
 if __name__ == "__main__":
     # 1. image files
     img_files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=None, random_order=False)
-    det = detector.Detector()
+
+    det = detector.Detector(model_filename, mean_value, model_input_shape, rp.MserRegionProposer())
     
     for img_file in img_files[100:]:
         # 2. image
         img = cv2.imread(img_file)
         
-        det.run(img, model_filename, mean_value, threshold=0.5, do_nms=True)
+        det.run(img, threshold=0.5, do_nms=True)
 
 
 
