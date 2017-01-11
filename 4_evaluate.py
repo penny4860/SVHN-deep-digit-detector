5a90a07069daf25903a1ce9719730e53c15cd244
jeongjoonsup <penny4860@gmail.com>
2017-01-11 22:40:04 +0900

    4_evaluate 드라이버 추가

--- 4_evaluate.py
+++ 4_evaluate.py
@@ -1,6 +1,5 @@
 #-*- coding: utf-8 -*-
 
-import progressbar
 import cv2
 import numpy as np
 
@@ -10,58 +9,7 @@ import digit_detector.file_io as file_io
 import digit_detector.preprocess as preproc
 import digit_detector.annotation as ann
 
-
-class Evaluator(object):
-    
-    def __init__(self, detector, annotator):
-        """
-        detector : Detector
-            instance of Detector class
-        """
-        self._detector = detector
-        self._annotator = annotator
-
-    def run(self, test_image_files):
-        
-        # setup the progress bar
-        widgets = ["Running for each Test image as gathering patches and its probabilities: ", 
-                   progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
-        pbar = progressbar.ProgressBar(maxval=len(test_image_files), widgets=widgets).start()
-        
-        n_detected = 0
-        n_truth = 0
-        n_true_positive = 0
-        
-        for i, image_file in enumerate(test_image_files):
-            test_image = cv2.imread(image_file)
-            
-            # 1. Get the detected boxes
-            detected_bbs, detected_probs_ = self._detector.run(test_image, threshold=0.5, do_nms=True, nms_threshold=0.1, show_result=False)
-
-            # 2. Get the true boxes
-            true_bbs, true_labels = annotator.get_boxes_and_labels(image_file)
-
-            # 3. Calc IOU between detected and true boxes
-            # Todo : class 로 모듈화하자.
-            overlaps = rp.calc_overlap(detected_bbs, true_bbs)
-            overlaps = np.max(overlaps, axis=0)
-            
-            n_detected += len(detected_bbs)
-            n_truth += len(true_bbs)
-            n_true_positive += len(overlaps[overlaps > 0.5])
-             
-            pbar.update(i)
-        pbar.finish()
-
-        recall = float(n_true_positive) / n_truth
-        precision = float(n_true_positive) / n_detected
-        f1_score = 2* (precision*recall) / (precision + recall)
-        self._print_msg(recall, precision, f1_score)
-        
-        return recall, precision, f1_score
-    
-    def _print_msg(self, recall, precision, f1_score):
-        print "recall value : {}, precision value : {}, f1_score : {}".format(recall, precision, f1_score)
+import digit_detector.evaluate as eval
         
     
 model_filename = "detector_model.hdf5"
@@ -72,18 +20,20 @@ ANNOTATION_FILE = "../datasets/svhn/train/digitStruct.json"
 
 if __name__ == "__main__":
     # 1. load test image files, annotation file
-    img_files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=1000, random_order=False)
+    img_files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=2, random_order=False)
     annotator = ann.SvhnAnnotation(ANNOTATION_FILE)
     
-    # 2. create detector
-    det = detector.Detector(model_filename, mean_value, model_input_shape, rp.MserRegionProposer(), preproc.GrayImgPreprocessor())
-
-    # 3. Evaluate average precision     
-    evaluator = Evaluator(det, annotator)
-    recall, precision, f1_score = evaluator.run(img_files)
+#     # 2. create detector
+#     det = detector.Detector(model_filename, mean_value, model_input_shape, rp.MserRegionProposer(), preproc.GrayImgPreprocessor())
+# 
+#     # 3. Evaluate average precision     
+#     evaluator = eval.Evaluator(det, annotator)
+#     recall, precision, f1_score = evaluator.run(img_files)
+#     # ecall value : 0.45329038196, precision value : 0.63141025641, f1_score : 0.527725689794
     
-
-
+    det = detector.Detector(None, mean_value, model_input_shape, rp.MserRegionProposer(), preproc.GrayImgPreprocessor())
+    evaluator = eval.Evaluator(det, annotator)
+    recall, precision, f1_score = evaluator.run(img_files)
 
 
 
