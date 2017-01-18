d896834ba4c65029a3ea6ca6506de90ae03820fe
jeongjoonsup <penny4860@gmail.com>
2017-01-18 23:25:15 +0900

    driver cleaning

--- 2_train.py
+++ 2_train.py
@@ -9,7 +9,9 @@ import digit_detector.train as train_
 DIR = ""
 NB_FILTERS = 32
 NB_EPOCH = 5
-SAVE_FILE = 'recognize_model.hdf5'
+
+DETECTOR_FILE = 'detector_model.hdf5'
+RECOGNIZER_FILE = 'recognize_model.hdf5'
 
 if __name__ == "__main__":
 
@@ -18,14 +20,18 @@ if __name__ == "__main__":
 
     images_val = file_io.FileHDF5().read(os.path.join(DIR, "val.hdf5"), "images")
     labels_val = file_io.FileHDF5().read(os.path.join(DIR, "val.hdf5"), "labels")
-    
-    X_train, X_val, Y_train, Y_val, mean_value = preproc.GrayImgTrainPreprocessor().run(images_train, labels_train, images_val, labels_val, 10)
-  
+
+    # Train detector
+    X_train, X_val, Y_train, Y_val, mean_value = preproc.GrayImgTrainPreprocessor().run(images_train, labels_train, images_val, labels_val, 2)
     print "mean value of the train images : {}".format(mean_value)    # 107.524
-    print "Train image shape is {}, and Validation image shape is {}".format(X_train.shape, X_val.shape)
-    
+    print "Train image shape is {}, and Validation image shape is {}".format(X_train.shape, X_val.shape)    # (457723, 32, 32, 1), (113430, 32, 32, 1)
+    train_.train_detector(X_train, X_val, Y_train, Y_val, nb_filters = NB_FILTERS, nb_epoch=NB_EPOCH, nb_classes=2, save_file=SAVE_FILE)
+
+    # Train recognizer
+    X_train, X_val, Y_train, Y_val, mean_value = preproc.GrayImgTrainPreprocessor().run(images_train, labels_train, images_val, labels_val, 10)
+    print "mean value of the train images : {}".format(mean_value)    # 112.833
+    print "Train image shape is {}, and Validation image shape is {}".format(X_train.shape, X_val.shape)    # (116913, 32, 32, 1), (29456, 32, 32, 1)
     train_.train_detector(X_train, X_val, Y_train, Y_val, nb_filters = NB_FILTERS, nb_epoch=NB_EPOCH, nb_classes=10, save_file=SAVE_FILE)
     # acc: 0.9541 - val_loss: 0.2125 - val_acc: 0.9452
-
 
 
