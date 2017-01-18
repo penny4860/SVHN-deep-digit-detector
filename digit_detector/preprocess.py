a83bddc2b5634472df40e9ec8b192faa104ed2d5
jeongjoonsup <penny4860@gmail.com>
2017-01-18 22:15:21 +0900

    GrayImgTrainPreprocessor() 에서 recognizer 도 처리 할 수 있도록 수정

--- digit_detector/preprocess.py
+++ digit_detector/preprocess.py
@@ -49,20 +49,28 @@ class GrayImgTrainPreprocessor(_TrainTimePreprocessor):
         X_train = np.array([self._to_gray(patch) for patch in images_train], dtype='float').reshape(-1, n_rows, n_cols, 1)
         X_val = np.array([self._to_gray(patch) for patch in images_val], dtype='float').reshape(-1, n_rows, n_cols, 1)
         
-        # 2. calc mean value
-        mean_value = X_train.mean()
-        
-        X_train -= mean_value
-        X_val -= mean_value
-    
         # convert class vectors to binary class matrices
         y_train = labels_train.astype('int')
         y_val = labels_val.astype('int')
+
         if nb_classes == 2:
             y_train[y_train > 0] = 1
             y_val[y_val > 0] = 1
+        elif nb_classes == 10:
+            X_train = X_train[y_train[:,0] > 0, :, :, :]
+            X_val = X_val[y_val[:,0] > 0, :, :, :]
+            y_train = y_train[y_train > 0]
+            y_val = y_val[y_val > 0]
+            y_train[y_train == 10] = 0
+            y_val[y_val == 10] = 0
+            
         Y_train = np_utils.to_categorical(y_train, nb_classes)
         Y_val = np_utils.to_categorical(y_val, nb_classes)
+
+        # 2. calc mean value
+        mean_value = X_train.mean()
+        X_train -= mean_value
+        X_val -= mean_value
      
         return X_train, X_val, Y_train, Y_val, mean_value
 
