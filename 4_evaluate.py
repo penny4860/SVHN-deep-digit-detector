59894c747d93a9901a063f1e06fe828ad727753c
jeongjoonsup <penny4860@gmail.com>
2017-01-22 20:17:56 +0900

    evalution client code modified

--- 4_evaluate.py
+++ 4_evaluate.py
@@ -7,40 +7,52 @@ import cv2
 import numpy as np
 
 import digit_detector.region_proposal as rp
-import digit_detector.detect as detector
+import digit_detector.detect as detect
 import digit_detector.file_io as file_io
 import digit_detector.preprocess as preproc
 import digit_detector.annotation as ann
-
 import digit_detector.evaluate as eval
-        
-    
+import digit_detector.classify as cls
+
+
 model_filename = "detector_model.hdf5"
-mean_value = 107.524
 model_input_shape = (32,32,1)
 DIR = '../datasets/svhn/train'
 ANNOTATION_FILE = "../datasets/svhn/train/digitStruct.json"
+
+detect_model = "detector_model.hdf5"
+recognize_model = "recognize_model.hdf5"
+mean_value_for_detector = 107.524
+mean_value_for_recognizer = 112.833
+
 
 if __name__ == "__main__":
     # 1. load test image files, annotation file
     img_files = file_io.list_files(directory=DIR, pattern="*.png", recursive_option=False, n_files_to_sample=1000, random_order=False)
     annotator = ann.SvhnAnnotation(ANNOTATION_FILE)
-    classifier = detector.CnnClassifier(model_filename, model_input_shape)
-    preprocessor = preproc.GrayImgPreprocessor(mean_value)
+    
+    preprocessor_for_detector = preproc.GrayImgPreprocessor(mean_value_for_detector)
+    preprocessor_for_recognizer = preproc.GrayImgPreprocessor(mean_value_for_recognizer)
+
+    detector = cls.CnnClassifier(detect_model, preprocessor_for_detector, model_input_shape)
+    recognizer = cls.CnnClassifier(recognize_model, preprocessor_for_recognizer, model_input_shape)
+
     proposer = rp.MserRegionProposer()
     
     # 2. create detector
-    det = detector.Detector(classifier, proposer, preprocessor)
-  
+    det = detect.DigitSpotter(detector, recognizer, proposer)
+     
     # 3. Evaluate average precision     
     evaluator = eval.Evaluator(det, annotator, rp.OverlapCalculator())
     recall, precision, f1_score = evaluator.run(img_files)
     # recall value : 0.513115508514, precision value : 0.714285714286, f1_score : 0.597214783074
     
     # 4. Evaluate MSER
-    classifier = detector.TrueBinaryClassifier(input_shape=(32,32))
+    detector = cls.TrueBinaryClassifier(input_shape=model_input_shape)
     preprocessor = preproc.NonePreprocessor()
-    det = detector.Detector(classifier, proposer, preprocessor)
+     
+    # Todo : detector, recognizer 를 none type 으로
+    det = detect.DigitSpotter(detector, recognizer, proposer)
     evaluator = eval.Evaluator(det, annotator, rp.OverlapCalculator())
     recall, precision, f1_score = evaluator.run(img_files, do_nms=False)
     #recall value : 0.630004601933, precision value : 0.0452547023239, f1_score : 0.0844436220084
