f43a5b2d82e308eb90888a5cbc7d1ddcd6607ae4
jeongjoonsup <penny4860@gmail.com>
2017-01-22 21:03:51 +0900

    augment option added

--- digit_detector/train.py
+++ digit_detector/train.py
@@ -3,11 +3,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
 
 import numpy as np
 
 
-def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size=128, nb_epoch=5, nb_classes=2, save_file='models/detector_model.hdf5'):
+def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size=128, nb_epoch=5, nb_classes=2, do_augment=False, save_file='models/detector_model.hdf5'):
     """ vgg-like deep convolutional network """
     
     np.random.seed(1337)  # for reproducibility
@@ -49,8 +50,20 @@ def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size
     model.compile(loss='categorical_crossentropy',
                   optimizer='adadelta',
                   metrics=['accuracy'])
-        
-    model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+    
+    if do_augment:
+        datagen = ImageDataGenerator(
+            rotation_range=20,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2)
+        datagen.fit(X_train)
+        model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                            samples_per_epoch=len(X_train), nb_epoch=nb_epoch,
+                            validation_data=(X_test, Y_test))
+    else:
+        model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
               verbose=1, validation_data=(X_test, Y_test))
     score = model.evaluate(X_test, Y_test, verbose=0)
     print('Test score:', score[0])
